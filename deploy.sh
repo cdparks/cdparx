79f045d0b9fca07205b156811a306fd397df7ae1
Chris Parks <christopher.daniel.parks@gmail.com>
2020-09-28 18:36:31 -0700

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eu
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$branch" != "main" ]; then
+  echo "Cannot deploy from branch $branch; switch to main first"
+  exit 2
+fi
+
+git branch -D gh-pages 2>/dev/null || true
+git checkout -b gh-pages
+git add -f .
+git commit -am "Deploy to gh-pages on $(date)"
+git push -u --force origin gh-pages:gh-pages
+git checkout main
