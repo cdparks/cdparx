3462b90e37102f43dd60c1780687e32c2a6ab8ca
Chris Parks <christopher.daniel.parks@gmail.com>
2023-07-16 09:47:58 -0700

    Add cohost, dev.sh -> serve.sh

--- serve.sh
+++ serve.sh
@@ -2,4 +2,13 @@
 
 set -eu
 
-python3 -m http.server --directory .
+python3 -m http.server --directory . &
+server=$!
+trap "kill $server" SIGINT SIGTERM EXIT
+sleep 1
+
+if [ `uname` == "Darwin" ]; then
+  open http://localhost:8000/index.html
+fi
+
+wait $server
